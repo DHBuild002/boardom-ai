0c0efd8d98252a807a3da86fa495bb08bfdb6eaf
DHBuild.002 <dhbuild.002@gmail.com>
2025-07-18 19:48:20 +0100

    Updated package-lock.json

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#10219F',
+        secondary: '#FE5F55',
+        accent: '#BDD5EA',
+        background: '#F7F7FF',
+        text: '#000000',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
