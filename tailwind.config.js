e7c623e37a11309d9aec36dc6e53bd1ce1eaf5f0
DHBuild.002 <dhbuild.002@gmail.com>
2025-07-18 21:05:38 +0100

    Fix complete styling configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./App.tsx",
+    "./index.tsx",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -21,4 +22,4 @@ export default {
     },
   },
   plugins: [],
-}
+}</content>
