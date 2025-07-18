05a32c96084a0e904a18bfe9d439a1819c4564d2
DHBuild.002 <dhbuild.002@gmail.com>
2025-07-18 22:15:41 +0100

    Added package-lock.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
 // postcss.config.js
-// For ES Modules (if your project uses import/export, like many Vite setups)
-import tailwindcss from '@tailwindcss/postcss';
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
   plugins: [
-    tailwindcss(), // Call it as a function
-    autoprefixer(), // Call it as a function
+    tailwindcss(), // Call as a function
+    autoprefixer(), // Call as a function
   ],
 };
