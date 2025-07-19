20136e02c67cddb5cdd2bf058703db7f067a7340
DHBuild.002 <dhbuild.002@gmail.com>
2025-07-19 16:49:04 +0100

    Fix PostCSS configuration syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 // postcss.config.js
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-import postcssNested from 'postcss-nested';
-
-export default {
-  plugins: [
-    tailwindcss(), // Call as a function
-    autoprefixer(), // Call as a function
-    postcssNested(), // Call as a function
-  ],
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
+    'postcss-nested': {},
+  },
 };
